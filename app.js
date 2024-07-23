a929f9195f15a264ada6c998f60de206f6706d04
Jisnakp <jisnab01@gmail.com>
2024-07-23 22:48:58 +0530

    signup api completed

--- app.js
+++ app.js
@@ -1,14 +1,52 @@
 const Express =require("express")
 const Mongoose =require("mongoose")
 const Bcrypt =require("bcrypt")
-const cors =require("cors")
+const Cors =require("cors")
 const Jwt =require("jsonwebtoken")
+const userModel=require("./models/users")
 
 
 let app= Express()
 
-app.get("/",(req,res)=>{
-    res.send("hello")
+app.use(Express.json())
+app.use(Cors())
+
+Mongoose.connect("mongodb+srv://jisnab01:jisnab01@cluster0.tnvz6eq.mongodb.net/blogAppDb?retryWrites=true&w=majority&appName=Cluster0")
+
+app.post("/signUp",async(req,res)=>{
+    
+     let input=req.body
+     let hashedPassword = Bcrypt.hashSync(req.body.password,10)
+        console.log(hashedPassword)
+        req.body.password=hashedPassword
+        
+
+        userModel.find({email:req.body.email}).then(
+
+             (items)=>{
+         
+                if (items.length>0) {
+
+                    res.json({"status":"emailid already exists"})
+                    
+                } else {
+        
+                    let result=new userModel(input)
+                    result.save()
+                   res.json({"status":"success"})
+                    
+                }
+             }
+
+        ).catch(
+
+        (error)=>{}
+        )
+
+       
+
+        
+     
 })
 
 app.listen(3030,()=>{
